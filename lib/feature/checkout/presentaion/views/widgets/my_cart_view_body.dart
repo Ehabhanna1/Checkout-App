57816d04e626f82e7c09d0473c9e40a19c4c01fb
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-03-21 14:20:39 +0200

    my cart view done

--- lib/feature/checkout/presentaion/views/widgets/my_cart_view_body.dart
+++ lib/feature/checkout/presentaion/views/widgets/my_cart_view_body.dart
@@ -12,20 +12,20 @@ class MyCartViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         children: [
-          SizedBox(height:25,),
-          Image.asset("assets/images/basket_image.png"),
-         const  SizedBox(height:30,),
+          SizedBox(height:15),
+          Expanded(child: Image.asset("assets/images/basket_image.png")),
+         const  SizedBox(height:15,),
           OrderInfoItem(title: 'Order Subtotal', value: r'$42.97',),
          const SizedBox(height:5,),
           OrderInfoItem(title: 'Discount', value: r'$0',),
          const SizedBox(height:5,),
           OrderInfoItem(title: 'Shipping', value: r'$8',),
-         const Divider(thickness: 2,height: 60,),
+         const Divider(thickness: 2,height: 50,),
           TotalPrice(title: 'Total', value: r'$50.97',),
-          const SizedBox(height:30,),
+          const SizedBox(height:20,),
         
          const CustomButton(),
-         const SizedBox(height:20),
+         const SizedBox(height:35),
 
 
 
