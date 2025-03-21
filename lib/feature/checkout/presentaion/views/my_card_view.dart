ccd1b3368a9353fe9886a7309c09581f0bf0f370
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-03-21 13:22:49 +0200

    add app bar

--- lib/feature/checkout/presentaion/views/my_card_view.dart
+++ lib/feature/checkout/presentaion/views/my_card_view.dart
@@ -1,12 +1,31 @@
+import 'package:checkout_payment/core/utils/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MyCardView extends StatelessWidget {
   const MyCardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return  Scaffold(
+
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text('My Card',
+     
+        style: AppStyles.style25),
+        centerTitle: true,
+        elevation: 0,
+        leading: Center(child: SvgPicture.asset("assets/images/arrow.svg",height: 20,)),
+        
+      ),
+      body: Column(
+        children: [
+          
+
+        ],
+      ),
+
     );
   }
 }
