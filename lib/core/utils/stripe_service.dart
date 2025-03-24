23c1bf4e6880a6bcde2f152ad3d2e985de64419f
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-03-24 14:18:04 +0200

    stripr sevice part 1

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,3 +1,22 @@
+import 'package:checkout_payment/core/utils/api_keys.dart';
+import 'package:checkout_payment/core/utils/api_service.dart';
+import 'package:checkout_payment/feature/checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout_payment/feature/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+
 class StripeService {
+  ApiService apiService = ApiService();
+  Future<PaymentIntentModel> createPaymentIntent (PaymentIntentInputModel paymentIntentInputModel) async{
+
+   var response = await  apiService.post(
+      body: paymentIntentInputModel.toJson(),
+
+     url: "https://api.stripe.com/v1/payment_intents",
+      token: ApiKeys.secretKey);
+
+      var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
+      return paymentIntentModel;
+
+    
+  }
   
 }
